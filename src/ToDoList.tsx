6ace1378c93170e0b1735c98864c9adbbe558013
leejongil1046 <100816520+leejongil1046@users.noreply.github.com>
2023-06-14 23:47:54 +0900

    Created Form with react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,22 +1,124 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setToDo] = useState("");
+//   const [toDoError, setToDoError] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setToDoError("");
+//     setToDo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     if (toDo.length < 10) {
+//       return setToDoError("To do should be longer.");
+//     }
+//     console.log("sumbit");
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+//         <button>Add</button>
+//         {toDoError !== "" ? toDoError : null}
+//       </form>
+//     </div>
+//   );
+// }
+
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
+}
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same." },
+        { shouldFocus: true }
+      );
+    }
+    //setError("extraError", { message: "Server offline." });
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  console.log(errors);
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required.",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed.",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message as string}</span>
+        <input
+          {...register("firstName", {
+            required: "Write here.",
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "No nicos allowed." : true,
+              noNick: (value) =>
+                value.includes("nick") ? "No nicks allowed." : true,
+            },
+          })}
+          placeholder="FirstName"
+        />
+        <span>{errors?.firstName?.message as string}</span>
+        <input
+          {...register("lastName", { required: "Write here." })}
+          placeholder="LastName"
+        />
+        <span>{errors?.lastName?.message as string}</span>
+        <input
+          {...register("username", { required: "Write here.", minLength: 10 })}
+          placeholder="Username"
+        />
+        <span>{errors?.username?.message as string}</span>
+        <input
+          {...register("password", { required: "Write here.", minLength: 5 })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message as string}</span>
+        <input
+          {...register("password1", {
+            required: "Password is required.",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.password1?.message as string}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message as string}</span>
       </form>
     </div>
   );
